c794ad5def019109ef84366a514652f0285385b1
skosana721 <wonganiskosana@gmail.com>
2021-03-12 10:47:10 +0200

    removed coded code

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-// const foo = (n) => {
-//   if (n < 0) {
-//     Math.abs(n);
-//   }
-//   if (n === 0) {
-//     return true;
-//   }
-//   if (n === 1) {
-//     return false;
-//   } else if({
-//     n - 2;
-//   }
-// };
-
-// foo();
 let x = [2, 3, 5];
 let results = x.reduce((acc, curr) => {
   acc *= curr;
